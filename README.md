685d109da1daeeba272e6853c2f51e6d8713062e
Jaquie <jaq.cat@protonmail.ch>
2022-07-23 08:32:43 -0300

    sus

---
+++ README.md
@@ -0,0 +1,3 @@
+Music downloader written in Dart
+
+`bin/id3_writer.dart` and `bin/id3_sizes.dart` based on [egoroof/browser-id3-writer](https://github.com/egoroof/browser-id3-writer)
---
+++ bin/ezmp.dart
@@ -0,0 +1,95 @@
+import 'dart:io';
+
+import 'package:dotenv/dotenv.dart' as dotenv;
+import 'package:spotify/spotify.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+/*import 'package:audiotagger/audiotagger.dart';*/
+
+class Youtube extends YoutubeExplode {
+  Future<VideoId> getSongId(String query) async {
+    SearchList vs;
+    vs = await search.getVideos(query);
+    return vs.first.id;
+  }
+
+  Future<Stream> downloadSongFromId(VideoId id) async {
+    final manifest = await videos.streamsClient.getManifest(id);
+    final sinfo = manifest.audioOnly.withHighestBitrate();
+    return videos.streamsClient.get(sinfo);
+  }
+
+  Future<Stream> downloadSong(String query) async => downloadSongFromId(await getSongId(query));
+}
+
+class Spotify extends SpotifyApi {
+  Spotify() : super(SpotifyApiCredentials(dotenv.env["SPOTIFY_CLIENT_ID"], dotenv.env["SPOTIFY_CLIENT_SECRET"]));
+
+  Future<Track?> getSongMetadata(String query) async {
+    Page page = (await search.get(query, types: [SearchType.track]).first()).first;
+    var item = page.items?.first;
+    if (item is Track) return item;
+    return null;
+  }
+}
+
+class Ezmp {
+  final Youtube yt;
+  final Spotify spotify;
+  /*final Audiotagger tagger;*/
+  Ezmp() :
+  yt = Youtube(),
+  spotify = Spotify()
+  // tagger = Audiotagger()
+  {
+  dotenv.load();
+  }
+
+  void downloadSong(String query) async {
+    String? songName;
+    String? spotifiedQuery;
+    /*Map<String, String> tags = {};*/
+
+    spotify.getSongMetadata(query).then((song) {
+      if (song != null) {
+        songName = song.name;
+        spotifiedQuery = "${songName} ${song.artists!.map((a) => a.name).join(", ")}";
+        var artworkAll = song.album!.images!;
+        var artwork = artworkAll[artworkAll.length ~/ 2];
+        /*tags = {*/
+          /*"title": songName!,*/
+          /*"artist": song.artists!.map((a) => a.name).join(", "),*/
+          /*"genre": song.artists!.first.genres!.first,*/
+          /*"trackNumber": song.trackNumber!.toString(),*/
+          /*"discNumber": song.discNumber!.toString(),*/
+          /*"album": song.album!.name!,*/
+          /*"albumArtist": song.album!.artists!.first.name!,*/
+          /*"year": song.album!.releaseDate!.substring(0, 4),*/
+          /*"artwork": artwork.url!,*/
+        /*};*/
+      }
+    });
+
+    if (spotifiedQuery == null) return;
+
+    var stream = await yt.downloadSong(spotifiedQuery!);
+
+    var fs = File("$songName.mp3").openWrite();
+    await stream.pipe(fs);
+    await fs.flush();
+    await fs.close();
+
+    /*await tagger.writeTagsFromMap(*/
+      /*path: "$songName.mp3",*/
+      /*tags: tags,*/
+    /*);*/
+  }
+}
+
+
+void main(List<String> arguments) async {
+  var ezmp = Ezmp();
+  var input = stdin.readLineSync();
+  if (input != null) {
+    ezmp.downloadSong(input);
+  }
+}
