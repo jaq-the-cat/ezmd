4f5d5da764b40d4c854c5e93b9c662e9cac572f4
Jaquie <jaq.cat@protonmail.ch>
2022-07-29 22:25:53 -0300

    added spotify playlist support

--- bin/ezmd.dart
+++ bin/ezmd.dart
@@ -19,6 +19,9 @@ class Youtube extends YoutubeExplode {
   Future<VideoId> getSongId(String query) async {
     VideoSearchList vs;
     vs = await search.search(query);
+    if (vs.isEmpty) {
+      throw VideoUnavailableException("$query turned up no results");
+    }
     return vs.first.id;
   }
 
@@ -30,6 +33,23 @@ class Youtube extends YoutubeExplode {
 
   Future<Stream> downloadSong(String query) async =>
       downloadSongFromId(await getSongId(query));
+
+  Future<void> downloadSongToMp3(String query, String tempname) async {
+    log("Downloading first Youtube result from query '$query'");
+    final stream = await downloadSong(query);
+    final tempstream = File("$tempname.webm").openWrite(mode: FileMode.write);
+    await stream.pipe(tempstream);
+    await tempstream.flush();
+    await tempstream.close();
+    log("Downlodaded .webm file to $tempname.webm");
+
+    // Convert webm to mp3 with ffmpeg
+    log("Converting .webm to .mp3");
+    final ffmpegargs =
+        "-i $tempname.webm -vn -ab 128k -ar 44100 -y $tempname.mp3";
+    await Process.run('ffmpeg', ffmpegargs.split(' '));
+    log("Converted");
+  }
 }
 
 class Spotify extends SpotifyApi {
@@ -37,7 +57,7 @@ class Spotify extends SpotifyApi {
       : super(SpotifyApiCredentials(dotenv.env["SPOTIFY_CLIENT_ID"],
             dotenv.env["SPOTIFY_CLIENT_SECRET"]));
 
-  Future<Track?> getSongMetadata(String query) async {
+  Future<Track?> _getSongMetadataByQuery(String query) async {
     Page page =
         (await search.get(query, types: [SearchType.track]).first()).first;
     final item = page.items?.first;
@@ -45,102 +65,116 @@ class Spotify extends SpotifyApi {
     return null;
   }
 
+  Future<Track?> _getSongMetadataById(String query) async {
+    Page page =
+        (await search.get(query, types: [SearchType.track]).first()).first;
+    final item = page.items?.first;
+    if (item is Track) return item;
+    return null;
+  }
+
+  Future<Map<String, String>?> getSongMetadata(
+      {String? query, String? id}) async {
+    Track? song;
+    if (query != null) {
+      song = await _getSongMetadataByQuery(query);
+    } else if (id != null) {
+      song = await _getSongMetadataById(id);
+    }
+    if (song == null) return null;
+    return extractSongMetadata(song);
+  }
+
+  Future<List<Track>?> getPlaylistTracks(String link) async {
+    final id = link.split('/').last.split('?').first;
+    final playlist = await Playlists(this).get(id);
+    final tracks = playlist.tracks?.itemsNative;
+    return tracks?.map((item) => Track.fromJson(item["track"])).toList();
+  }
+
   Future<List<String>?> getGenres(ArtistSimple? artist) async {
     if (artist == null) return null;
-    final artistFull = await Artists(spotify).get(artist.id!);
+    final artistFull = await Artists(this).get(artist.id!);
     return artistFull.genres;
   }
+}
+
+Future<Map<String, String>?> extractSongMetadata(Track? song) async {
+  if (song == null) return null;
+  final artworkAll = song.album!.images!;
+  // get middle artwork in case theres a lot
+  final artwork = artworkAll[artworkAll.length ~/ 2];
+  final genres = await spotify.getGenres(song.artists!.first);
+  String genre = "";
+  if (genres != null && genres.isNotEmpty) {
+    genre = genres.first;
+  }
+  return {
+    // data to be transfered to other functio
+    "query": "${song.artists!.first.name} - ${song.name!}",
+
+    // actual tags
+    "title": song.name!,
+    // artists name must be separated by "/" (ID3v2 standard)
+    "artist": song.artists!.map((a) => a.name).join("/"),
+    "album": song.album!.name!,
+    "year": song.album!.releaseDate!.substring(0, 4),
+    "track": song.trackNumber.toString(),
+    "artwork": artwork.url ?? "",
+    "duration": song.durationMs.toString(),
+    "genre": genre,
+  };
 }
 
 final yt = Youtube();
 final spotify = Spotify();
 
-void downloadSongTo(String query, String path, {bool lyrics = false}) async {
-  log("Downloading '$query' to '$path'");
+void downloadSongFromQuery(String query, String outPath,
+    {bool lyrics = false}) async {
+  log("Downloading '$query' to '$outPath'");
 
-  String? songName;
-  String? correctedQuery;
-  String? finalFilename;
-  TagList frames = TagList();
+  String? properQuery;
 
-  final song = await spotify.getSongMetadata(query);
-  if (song != null) {
-    songName = song.name;
-    log("Found song: $songName");
-
-    // convert query to a nicer format
-    correctedQuery =
-        "${song.artists!.map((a) => a.name).join(", ")} - $songName";
-    final artworkAll = song.album!.images!;
-    // get middle artwork in case theres a lot
-    final artwork = artworkAll[artworkAll.length ~/ 2];
-    final genres = spotify.getGenres(song.artists!.first);
-    frames = TagList.fromMap({
-      "title": songName!,
-      // artists name must be separated by "/" (ID3v2 standard)
-      "artist": song.artists!.map((a) => a.name).join("/"),
-      "album": song.album!.name!,
-      "year": song.album!.releaseDate!.substring(0, 4),
-      "track": song.trackNumber.toString(),
-      "artwork": artwork.url ?? "",
-      "duration": song.durationMs.toString(),
-      "genre": ((await genres)?.first) ?? "",
-    });
-    log("Found metadata: $frames");
-    finalFilename = "$path/${song.artists!.first.name} - $songName";
+  final tags = await spotify.getSongMetadata(query: query);
+  if (tags == null) {
+    properQuery = query;
   } else {
-    stderr.writeln(
-        "Couldn't find song on Spotify, using query as filename instead");
-    correctedQuery = query;
-    finalFilename = "$path/$correctedQuery";
+    properQuery = tags.remove("query")!;
   }
 
   // appending " Lyrics" to the query can sometimes improve music search results in Youtube
-  if (lyrics) correctedQuery = correctedQuery + " Lyrics";
-
-  log("Downloading first Youtube result from query '$correctedQuery'");
-  final stream = await yt.downloadSong(correctedQuery);
-  final tempid = uuid.v4();
-  final tempstream = File("/tmp/$tempid.webm").openWrite(mode: FileMode.write);
-  await stream.pipe(tempstream);
-  await tempstream.flush();
-  await tempstream.close();
-  log("Downlodaded .webm file to /tmp/$tempid.webm");
-
-  // Convert webm to mp3 with ffmpeg
-  log("Converting .webm to .mp3");
-  final ffmpegargs =
-      "-i /tmp/$tempid.webm -vn -ab 128k -ar 44100 -y /tmp/$tempid.mp3";
-  await Process.run('ffmpeg', ffmpegargs.split(' '));
-  log("Converted");
-
-  log("Writing tags to $finalFilename.mp3");
-  final mp3Bytes = File("/tmp/$tempid.mp3").readAsBytesSync();
-  final f = File("$finalFilename.mp3");
-  f.writeAsBytesSync(await makeId3v2(frames) + mp3Bytes);
-
-  log("Downlodaded '$correctedQuery'");
+  if (lyrics) properQuery = properQuery + " Lyrics";
+  downloadAndAddTags(query, outPath, TagList.fromMap(tags ?? {}));
 }
 
-String getMusicFolder() {
-  String? musicPath;
+void downloadSongFromTrack(Track? song, String outPath,
+    {bool lyrics = false}) async {
+  if (song == null) return;
 
-  if (Platform.isMacOS || Platform.isLinux) {
-    musicPath = path.join(Platform.environment["HOME"]!, "Music");
-  } else if (Platform.isWindows) {
-    musicPath = path.join(Platform.environment["UserProfile"]!, "Music");
+  final tags = await extractSongMetadata(song);
+  String query = tags!.remove("query")!;
+
+  // appending " Lyrics" to the query can sometimes improve music search results in Youtube
+  if (lyrics) query = query + " Lyrics";
+  downloadAndAddTags(query, outPath, TagList.fromMap(tags));
+}
+
+Future<void> downloadAndAddTags(
+    String query, String outPath, TagList tags) async {
+  String tempname = "/tmp/${uuid.v4()}";
+  try {
+    await yt.downloadSongToMp3(query, tempname);
+  } catch (e) {
+    throw Exception("Failed to download $query");
   }
 
-  // If failed to detect platform
-  if (musicPath == null) {
-    stderr.writeln("[!] Failed to detect platform");
+  log("Writing tags to $query.mp3");
+  final mp3Bytes = File("$tempname.mp3").readAsBytesSync();
+  String filename = path.join(outPath, query.replaceAll('/', '-'));
+  final f = File("$filename.mp3");
+  f.writeAsBytesSync(await makeId3v2(tags) + mp3Bytes);
 
-    // Read path directly from the user
-    stdout.write("Target path/> ");
-    musicPath = stdin.readLineSync();
-  }
-
-  return musicPath ?? "";
+  log("Downlodaded '$query'");
 }
 
 void main(List<String> arguments) async {
@@ -153,10 +187,11 @@ void main(List<String> arguments) async {
       abbr: "o", help: "Target folder", defaultsTo: null);
   parser.addOption("intype",
       abbr: "t",
-      help:
-          "Input type. 'file' indicates the input is a file to be read line by "
-          "line. 'query' means the input will be interpreted as a single query",
-      allowed: ["file", "query"],
+      help: "How to interpret the input\n"
+          "file: input is a file to be read line by line.\n"
+          "query: input will be interpreted as a single query\n"
+          "spotify: input will be interpreted as links to a spotify playlist",
+      allowed: ["file", "query", "spotify"],
       defaultsTo: "query");
   parser.addFlag("lyrics",
       abbr: "l",
@@ -171,7 +206,7 @@ void main(List<String> arguments) async {
   // Get target folder
   outPath = results["folder"] ?? getMusicFolder();
   if (outPath == null || outPath.isEmpty) {
-    stderr.writeln("Unable to get target path");
+    stderr.writeln("Unable to get output path");
     return;
   }
 
@@ -179,14 +214,41 @@ void main(List<String> arguments) async {
   switch (results["intype"]) {
     case "query":
       query = results.rest.join(" ");
-      downloadSongTo(query, outPath);
+      downloadSongFromQuery(query, outPath);
       break;
     case "file":
-      for (String filename in results.rest) {
-        for (String query in File(filename).readAsLinesSync()) {
-          downloadSongTo(query.trim(), outPath);
+      for (final filename in results.rest) {
+        for (final query in File(filename).readAsLinesSync()) {
+          try {
+            downloadSongFromQuery(query.trim(), outPath);
+          } catch (e) {
+            continue;
+          }
+        }
+      }
+      break;
+    case "spotify":
+      for (final link in results.rest) {
+        for (final song in (await spotify.getPlaylistTracks(link)) ?? []) {
+          try {
+            downloadSongFromTrack(song, outPath);
+          } catch (e) {
+            continue;
+          }
         }
       }
       break;
   }
+}
+
+String? getMusicFolder() {
+  String? musicPath;
+
+  if (Platform.isMacOS || Platform.isLinux) {
+    musicPath = path.join(Platform.environment["HOME"]!, "Music");
+  } else if (Platform.isWindows) {
+    musicPath = path.join(Platform.environment["UserProfile"]!, "Music");
+  }
+
+  return musicPath;
 }
